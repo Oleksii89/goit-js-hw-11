c6489f712c2c71da03ed16c1262b4242900f9f03
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-08-16 15:29:07 +0300

    update

--- src/images-api.js
+++ src/images-api.js
@@ -6,37 +6,22 @@ const BASE_URL = 'https://pixabay.com/api/';
 const NEW_URL =
   'https://pixabay.com/api/?key=38828352-a415a8248b03313c93049703f&q=yellow+flowers&image_type=photo';
 
-function fetchImages() {
+async function fetchImages() {
   const params = new URLSearchParams({
     q: 'yellow+flowers',
     image_type: 'photo',
-    // orientation: 'horizontal',
-    // safesearch: true,
+    orientation: 'horizontal',
+    safesearch: true,
   });
-  // return axios.get(`${BASE_URL}?${params}`).then(resp => {
-  //   console.log(resp);
-  // });
-  return axios
-    .get(
-      `https://pixabay.com/api/?key=38828352-a415a8248b03313c93049703f&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true`
-    )
-    .then(resp => {
-      console.log(resp);
+
+  const response = await axios
+    .get(`${BASE_URL}?key=${API_KEY}&${params}`)
+    .then(response => {
+      // return response.data;
+      console.log(response.data);
     });
-  // return fetch(`${NEW_URL}`).then(resp => {
-  //   if (!resp.ok) {
-  //     throw new Error(resp.statusText);
-  //   }
-  //   console.log(resp.json());
-  // });
 }
 
-// async function fetchImages() {
-//   const response = await axios.get(
-//     `$https://pixabay.com/api/?key=38828352-a415a8248b03313c93049703f&q=yellow+flowers&image_type=photo`
-//   );
-//   console.log(response.data);
-// }
 fetchImages().catch(error => {
   console.log(error);
 });
