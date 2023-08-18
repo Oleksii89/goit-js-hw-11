2ca0a6a30301f77c6160e238788b4f3ebef0f3d0
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-08-18 19:11:26 +0300

    update

--- src/js/images-api.js
+++ src/js/images-api.js
@@ -3,13 +3,15 @@ const API_KEY = '38828352-a415a8248b03313c93049703f';
 
 const BASE_URL = 'https://pixabay.com/api/';
 
-async function fetchImages(query, page) {
+async function fetchImages(query, page, perPage) {
   const params = new URLSearchParams({
-    q: 'yellow+flowers',
+    q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    per_page: 40,
+    page: page,
+    per_page: perPage,
+    query: query,
   });
 
   const response = await axios.get(`${BASE_URL}?key=${API_KEY}&${params}`);
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,71 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './images-api';
 
 const refs = {
   gallery: document.querySelector('.gallery '),
   loadMore: document.querySelector('.js-load-more'),
+  searchForm: document.querySelector('.search-form'),
 };
 
 let query = '';
 let page = 1;
+let simpleLightBox;
+const perPage = 40;
 
-fetchImages()
+refs.searchForm.addEventListener('submit', onSearchForm);
+
+refs.loadMore.addEventListener('click', onLoadMore);
+
+fetchImages(query, page, perPage)
   .then(response => {
     refs.gallery.insertAdjacentHTML('beforeend', createMarkup(response.hits));
-    // console.log(createMarkup(response.hits));
     console.log(response);
+
+    const totalPages = Math.ceil(response.totalHits / perPage);
+
+    simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+    Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
+
+    if (page < totalPages) {
+      refs.loadMore.classList.replace('load-more-hidden', 'load-more');
+      refs.loadMore.disabled = false;
+    } else {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      refs.loadMore.classList.replace('load-more', 'load-more-hidden');
+    }
   })
   .catch(onFetchError);
+
+// function onSearchForm(evt) {
+//   evt.preventDefault();
+//   page = 1;
+//   console.log(evt.currentTarget.elements);
+//   query = evt.currentTarget.elements.searchQuery.value.trim();
+//   gallery.innerHTML = '';
+
+//   if (query === '') {
+//     Notiflix.Notify.failure(
+//       'The search string cannot be empty. Please specify your search query.'
+//     );
+//     return;
+//   }
+// }
+
+function onLoadMore() {
+  page += 1;
+  refs.loadMore.disabled = true;
+
+  fetchImages(query, page, perPage).then(response => {
+    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(response.hits));
+    refs.loadMore.disabled = false;
+
+    console.log(response);
+  });
+}
 
 function createMarkup(arr) {
   return arr
