ab2ebe4e0d6cfcbe260b8e5192f8bdff42b46f07
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-08-23 13:47:45 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -137,3 +137,12 @@ function onFetchError(error) {
     }
   );
 }
+
+arrowTop.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  // після scrollTo відбудеться подія "scroll", тому стрілка автоматично сховається
+};
+
+window.addEventListener('scroll', function () {
+  arrowTop.hidden = scrollY < document.documentElement.clientHeight;
+});
